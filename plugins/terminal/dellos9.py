a311cf542d0180739f64ce528a5362af8e87332d
Justas Balcas <juztas@gmail.com>
2023-11-05 07:52:56 -0800

    Pylint

--- plugins/terminal/dellos9.py
+++ plugins/terminal/dellos9.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import json
 # Copyright: Contributors to the Ansible project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 import re
-import json
 
-from ansible.module_utils._text import to_text, to_bytes
-from ansible.plugins.terminal import TerminalBase
 from ansible.errors import AnsibleConnectionFailure
+from ansible.module_utils._text import to_bytes, to_text
+from ansible.plugins.terminal import TerminalBase
+
 
 class TerminalModule(TerminalBase):
     """
@@ -17,20 +18,22 @@ class TerminalModule(TerminalBase):
     """
 
     terminal_stdout_re = [
-        re.compile(br"[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$"),
-        re.compile(br"\[\w+\@[\w\-\.]+(?: [^\]])\] ?[>#\$] ?$")
+        re.compile(rb"[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$"),
+        re.compile(rb"\[\w+\@[\w\-\.]+(?: [^\]])\] ?[>#\$] ?$"),
     ]
 
     terminal_stderr_re = [
-        re.compile(br"% ?Error: (?:(?!\bdoes not exist\b)(?!\balready exists\b)(?!\bHost not found\b)(?!\bnot active\b).)*\n"),
-        re.compile(br"% ?Bad secret"),
-        re.compile(br"invalid input", re.I),
-        re.compile(br"(?:incomplete|ambiguous) command", re.I),
-        re.compile(br"connection timed out", re.I),
-        re.compile(br"'[^']' +returned error code: ?\d+"),
+        re.compile(
+            rb"% ?Error: (?:(?!\bdoes not exist\b)(?!\balready exists\b)(?!\bHost not found\b)(?!\bnot active\b).)*\n"
+        ),
+        re.compile(rb"% ?Bad secret"),
+        re.compile(rb"invalid input", re.I),
+        re.compile(rb"(?:incomplete|ambiguous) command", re.I),
+        re.compile(rb"connection timed out", re.I),
+        re.compile(rb"'[^']' +returned error code: ?\d+"),
     ]
 
-    terminal_initial_prompt = br"\[y/n\]:"
+    terminal_initial_prompt = rb"\[y/n\]:"
 
     terminal_initial_answer = b"y"
 
@@ -56,9 +59,11 @@ class TerminalModule(TerminalBase):
         terminal parameters such as disabling paging for instance.
         """
         try:
-            self._exec_cli_command(b'terminal length 0')
+            self._exec_cli_command(b"terminal length 0")
         except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters') from AnsibleConnectionFailure
+            raise AnsibleConnectionFailure(
+                "unable to set terminal parameters"
+            ) from AnsibleConnectionFailure
 
     def on_close_shell(self):
         """Called before the connection is closed
@@ -80,18 +85,22 @@ class TerminalModule(TerminalBase):
         of the terminal plugin to actually do the privilege escalation such
         as entering `enable` mode for instance
         """
-        if self._get_prompt().endswith(b'#'):
+        if self._get_prompt().endswith(b"#"):
             return
 
-        cmd = {u'command': u'enable'}
+        cmd = {"command": "enable"}
         if passwd:
-            cmd[u'prompt'] = to_text(r"[\r\n]?password: $", errors='surrogate_or_strict')
-            cmd[u'answer'] = passwd
+            cmd["prompt"] = to_text(r"[\r\n]?password: $", errors="surrogate_or_strict")
+            cmd["answer"] = passwd
 
         try:
-            self._exec_cli_command(to_bytes(json.dumps(cmd), errors='surrogate_or_strict'))
+            self._exec_cli_command(
+                to_bytes(json.dumps(cmd), errors="surrogate_or_strict")
+            )
         except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to elevate privilege to enable mode') from AnsibleConnectionFailure
+            raise AnsibleConnectionFailure(
+                "unable to elevate privilege to enable mode"
+            ) from AnsibleConnectionFailure
 
     def on_unbecome(self):
         """Called when privilege deescalation is requested
@@ -105,9 +114,9 @@ class TerminalModule(TerminalBase):
             # if prompt is None most likely the terminal is hung up at a prompt
             return
 
-        if prompt.strip().endswith(b')#'):
-            self._exec_cli_command(b'end')
-            self._exec_cli_command(b'disable')
+        if prompt.strip().endswith(b")#"):
+            self._exec_cli_command(b"end")
+            self._exec_cli_command(b"disable")
 
-        elif prompt.endswith(b'#'):
-            self._exec_cli_command(b'disable')
+        elif prompt.endswith(b"#"):
+            self._exec_cli_command(b"disable")
